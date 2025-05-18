8db2ff8fd7c899b08de0623bc4f3a3dda02937fa
gdhillon2 <gman.dhillon@gmail.com>
2025-05-18 11:02:36 -0700

    removed drawing circles and squares

--- frontend/include/drawing/Shapes.h
+++ frontend/include/drawing/Shapes.h
@@ -16,8 +16,6 @@ void drawLine(
     int endY,
     int add_to_stack
 );
-void drawCircle(SDL_Renderer* renderer, int centerX, int centerY, int radius);
-void drawSquare(SDL_Renderer* renderer, int startX, int startY, int endX, int endY);
 void erase(
     int startX,
     int startY,
